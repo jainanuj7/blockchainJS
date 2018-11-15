ab1026677885172fcfbf6cf6d3231715d07843fb
Anuj Jain <lookforaj@gmail.com>
2018-11-16 03:14:38 +0530

    Created Blockchain Prototype
    Added prototype functions

---
+++ dev/test.js
@@ -0,0 +1,39 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+// const previousBlockHash = 'e4a21352101f71eaf53326984eaad9ie8f6795deffbfb86bab4c6bcdd6854bd4';
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: '7H487FH45',
+//         recipient: 'LKTRH89H90'
+//     },
+//     {
+//         amount: 130,
+//         sender: '7H487RGFERFH45',
+//         recipient: 'LKERFERTRH89H90'
+//     },
+//     {
+//         amount: 150,
+//         sender: '7H487F324H45',
+//         recipient: 'L23423KTRH89H90'
+//     }
+// ];
+// const nonce = 100;
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 36578));
+
+// bitcoin.createNewBlock(2389, 'SGF4YGERFYUE', '7AHDAS87DG');
+
+// bitcoin.createNewTransaction(100, 'ALEXWREF', 'JENNGFGV');
+
+// bitcoin.createNewBlock(239, 'SGF4ERFYUE', '7AS87DG');
+
+// bitcoin.createNewTransaction(600, 'ALEXWREF', 'JENNGFGV');
+// bitcoin.createNewTransaction(800, 'ALEXWREF', 'JENNGFGV');
+// bitcoin.createNewTransaction(170, 'ALEXWREF', 'JENNGFGV');
+
+// bitcoin.createNewBlock(349, 'SGFFF4ERFYUE', 'ASD47AS87DG');
+
+
+console.log(bitcoin);
