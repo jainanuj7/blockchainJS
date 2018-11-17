3ecf17bfa8a4b18c6bc40a3e9b23b27f12957fa3
Anuj Jain <lookforaj@gmail.com>
2018-11-18 00:32:55 +0530

    Added /mine endpoint
    Added /mine and assigned uuid to the node network

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+
+const nodeAddress = uuid().split('-').join('');
 
 const bitcoin = new Blockchain();
 
@@ -15,9 +18,25 @@ app.get('/blockchain', function (req, res) {
 
 app.post('/transaction', function (req, res) {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    
+    res.json({note: `Transaction will be added in block ${blockIndex}.`});
 })
 app.get('/mine', function (req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: 'New block mined successfully',
+        block: newBlock
+    });
+    
   
 })
  
